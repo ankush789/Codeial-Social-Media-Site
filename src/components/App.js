38252cbbec41c3ac37fa61aeea15a610cff6ddfa
ankush789 <56834051+ankush789@users.noreply.github.com>
2021-05-23 20:09:53 +0530

    Added posts to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { fetchPosts } from '../actions/posts';
 class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
   render() {
     console.log('Props', this.props);
     return <div>App</div>;
